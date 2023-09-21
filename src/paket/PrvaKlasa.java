528fbb989539108663dc93d4a3692441c762ecea
Gimnazija 20 <Gimnazija 20@DESKTOP-SO1M66G>
2023-09-21 08:56:03 +0200

    Commit

--- src/paket/PrvaKlasa.java
+++ src/paket/PrvaKlasa.java
@@ -1,6 +1,49 @@
 package paket;
 
-public class PrvaKlasa {
+public class Point{
+	private int x;
+	private int y;
+	private boolean selected;
+
+	public Point() {
+	
+	}
+	
+	public Point(int x, int y) {
+		this.x=x;
+		this.y=y;
+	}
+	
+	public Point(int x, int y, boolean selected) {
+		this.x=x;
+		this.y=y;
+	
+			
+	}
+	
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public boolean isSelected() {
+		return selected;
+	}
+
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
 
 }
-// prvi kometnar
