13eb6343725655af2630542f7c570d109dd673a4
promajjja <promajjja@gmail.com>
2023-09-21 09:59:50 +0200

    initial commit

---
+++ src/paket/Duz.java
@@ -0,0 +1,20 @@
+package paket;
+
+public class Duz {
+	public Point pocetnaTacka;
+	public Point krajnjaTacka;
+	
+	public Duz(Point pocetnaTacka, Point krajnjaTacka) {
+		this.pocetnaTacka = pocetnaTacka;
+		this.krajnjaTacka = krajnjaTacka;
+	}
+	
+	public Point getPocetnaTacka() {
+		return pocetnaTacka;
+		}
+
+
+
+}
+
+
