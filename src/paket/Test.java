b1bde5f411310317e4108c1f8487db5e5a6e0902
Gimnazija 20 <Gimnazija 20@DESKTOP-SO1M66G>
2023-09-21 09:28:53 +0200

    initial

--- src/paket/Test.java
+++ src/paket/Test.java
@@ -4,16 +4,15 @@ public class Test {
 
 	public static void main(String[] args) {
 		Point point1 = new Point(3, 4, true);
-		
-		//point.x = 10;
-		//point.y = 5;
-		
+
+		// point.x = 10;
+		// point.y = 5;
+
 		point1.setX(10);
 		point1.setY(5);
-		
 
 		System.out.println("X koordinata tacke je: " + point1.getX());
-		System.out.println("Y koordinata tacke je: " + point1.getY()); 
+		System.out.println("Y koordinata tacke je: " + point1.getY());
 		System.out.println("Da li je selektovana " + point1.isSelected());
 
 	}
